1a53f320f89f7ad5b549022219ef2e28e8b10fe5
Enrik <enrik.b94@live.it>
2023-11-29 18:50:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,27 +1,75 @@
-//create web server
-var express = require('express');
-var app = express();
-//use the express middleware for serving static files
-app.use(express.static(__dirname));
-//start the web server on port 3000
-app.listen(3000);
-console.log('Listening on port 3000');
-//create socket server
-var io = require('socket.io').listen(3001);
-//handle incoming connections from clients
-io.sockets.on('connection', function (socket) {
-  //once a client has connected, we expect to get a ping from them saying what room they want to join
-  socket.on('room', function(room) {
-    socket.join(room);
-  });
-  // when the server receives a ping from the client, send them the
-  // current time
-  socket.on('ping', function () {
-    socket.emit('pong', { time: new Date().toJSON() });
-  });
-  // when the server receives a comment, it sends it to the other person in the room.
-  socket.on('comment', function (data) {
-    socket.broadcast.to(data.room).emit('comment', data);
-  });
-}
-);
+//Create web server
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+const User = require('../models/user');
+const { ensureAuthenticated } = require('../config/auth');
+
+// Add comment to a post
+router.post('/add/:id', ensureAuthenticated, (req, res) => {
+    const newComment = new Comment({
+        text: req.body.text,
+        user: req.user.id
+    });
+    newComment.save()
+        .then(comment => {
+            Post.findOne({ _id: req.params.id })
+                .then(post => {
+                    post.comments.push(comment._id);
+                    post.save()
+                        .then(post => {
+                            res.redirect(`/posts/${req.params.id}`);
+                        });
+                });
+        });
+});
+
+// Delete comment
+router.delete('/:id', ensureAuthenticated, (req, res) => {
+    Comment.findOneAndDelete({ _id: req.params.id })
+        .then(comment => {
+            Post.findOne({ comments: req.params.id })
+                .then(post => {
+                    post.comments.remove(req.params.id);
+                    post.save()
+                        .then(post => {
+                            res.redirect(`/posts/${post._id}`);
+                        });
+                });
+        });
+});
+
+// Edit comment
+router.put('/:id', ensureAuthenticated, (req, res) => {
+    Comment.findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then(comment => {
+            res.redirect(`/posts/${comment.post}`);
+        });
+});
+
+// Like comment
+router.post('/:id/like', ensureAuthenticated, (req, res) => {
+    Comment.findOne({ _id: req.params.id })
+        .then(comment => {
+            comment.likes.push(req.user.id);
+            comment.save()
+                .then(comment => {
+                    res.redirect(`/posts/${comment.post}`);
+                });
+        });
+});
+
+// Dislike comment
+router.post('/:id/dislike', ensureAuthenticated, (req, res) => {
+    Comment.findOne({ _id: req.params.id })
+        .then(comment => {
+            comment.likes.remove(req.user.id);
+            comment.save()
+                .then(comment => {
+                    res.redirect(`/posts/${comment.post}`);
+                });
+        });
+});
+
+module.exports = router;
